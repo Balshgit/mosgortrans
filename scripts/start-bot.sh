734bbd836edc8ec1be5e936ff1770ed6aa6a8976
Dmitry Afanasyev <Balshbox@gmail.com>
2022-08-16 11:48:21 +0300

    app with uvicorn

--- scripts/start-bot.sh
+++ scripts/start-bot.sh
@@ -4,4 +4,4 @@
 echo "starting the bot"
 cd /opt/mosgortrans \
 && source /home/balsh/.cache/pypoetry/virtualenvs/mosgortrans-3eZxMcY3-py3.10/bin/activate \
-&& python main.py
+&& uvicorn app.main:create_app --host localhost --port 8084 --reload --factory
