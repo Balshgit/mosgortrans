e0ee4eade7e06d75e4332f9a8fd2672f34f9057c
Dmitry Afanasyev <Balshbox@gmail.com>
2022-08-16 13:26:51 +0300

    change app to core imports

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Update
 from aiogram.utils.executor import start_polling, start_webhook
 from aiohttp import web
-from app.core.bot import bot, dispatcher
-from app.core.logger import logger
-from app.core.scheduler import asyncio_schedule
-from app.settings import (
+from core.bot import bot, dispatcher
+from core.logger import logger
+from core.scheduler import asyncio_schedule
+from settings import (
     START_WITH_WEBHOOK,
     WEBAPP_HOST,
     WEBAPP_PORT,
