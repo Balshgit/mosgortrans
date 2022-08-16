9680e291dce274a7afd66decec3ccd7e352f634d
Dmitry Afanasyev <Balshbox@gmail.com>
2022-08-16 14:07:26 +0300

    change app to core imports

--- scripts/start-bot.sh
+++ scripts/start-bot.sh
@@ -2,6 +2,6 @@
 
 
 echo "starting the bot"
-cd /opt/mosgortrans \
+cd /opt/mosgortrans/app \
 && source /home/balsh/.cache/pypoetry/virtualenvs/mosgortrans-3eZxMcY3-py3.10/bin/activate \
-&& gunicorn app.main:create_app --bind prod-server.lan:8084 --worker-class aiohttp.GunicornWebWorker
+&& gunicorn main:create_app --bind prod-server.lan:8084 --worker-class aiohttp.GunicornWebWorker
