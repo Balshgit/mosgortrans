8253e8632b7c3fbde31e7e13ffe7527590cc1bb3
Dmitry Afanasyev <Balshbox@gmail.com>
2022-08-26 00:16:23 +0300

    remove gunicorn logs

--- scripts/start-bot.sh
+++ scripts/start-bot.sh
@@ -3,13 +3,8 @@
 echo "starting the bot"
 
 gunicorn main:create_app \
-  --bind 127.0.0.1:8084 \
+  --bind 0.0.0.0:8084 \
   --worker-class aiohttp.GunicornWebWorker \
   --timeout 150 \
   --max-requests 2000 \
-  --max-requests-jitter 400 \
-  --chdir "/app/logs" \
-  --log-level info \
-  --error-logfile "/app/logs/gunicorn_err.log" \
-  --worker-tmp-dir "/tmp" \
-  --pid "/app/logs/gunicorn_pid"
+  --max-requests-jitter 400
